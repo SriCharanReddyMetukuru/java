986b53f7eebe2a0a9e7dbc235afc666e22eb272e
SriCharanReddyMetukuru <metukurusricharanreddy@gmail.com>
2025-02-15 17:27:13 +0530

    Day 13

---
+++ wipro_java_project/src/com/wipro/java/collection/treemap/TreeMapCreation.java
@@ -0,0 +1,15 @@
+package com.wipro.java.collection.treemap;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class TreeMapCreation {
+    public static void main(String args[]) {
+        
+        // Create a TreeMap of Strings (keys) and Integers (values)
+        TreeMap<String, Integer> treeMap = new TreeMap<>();
+        
+        // Displaying the TreeMap
+        System.out.println("TreeMap elements: " + treeMap);
+    }
+}
